76f02052da2e0893427849e2aa8372b4b7df11b8
FettahAud <abooo2003adoany@gmail.com>
2021-11-21 21:36:12 +0300

    init

---
+++ app.js
@@ -0,0 +1,19 @@
+const nav = document.querySelectorAll('.nav li');
+const titleH1 = document.querySelector('.title__h1 span');
+const titleH2 = document.querySelector('.title__h2 span');
+const lorem = document.querySelector('.lorem');
+const btn = document.querySelector('.btn');
+const reset = document.querySelector('.reset');
+
+window.addEventListener('load', () => {
+    const animation = gsap.timeline()
+        .from(nav, { yPercent: -200, stagger: 0.2 })
+        .from(titleH2, { yPercent: -100 })
+        .from(titleH1, { yPercent: -100 }, '-=0.3')
+        .from(lorem, { opacity: 0, duration: 0.4 })
+        .from(btn, { scale: 0, ease: 'back.out(1.7)' });
+
+    reset.addEventListener('click', () => {
+        animation.restart();
+    })
+})
